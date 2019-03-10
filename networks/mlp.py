ec54e4c9eb80a282417a89fdf3588c720706fd25
DQle38 <ldk308@skku.edu>
2019-03-10 22:55:59 +0900

    baseline: ewc network modified to 1 hidden layer with 400 units.

--- networks/mlp.py
+++ networks/mlp.py
@@ -1,6 +1,6 @@
 import sys
 import torch
-
+import torch.nn.functional as F
 # import utils
 
 class Net(torch.nn.Module):
@@ -10,10 +10,10 @@ class Net(torch.nn.Module):
 
         ncha,size,_=inputsize
         self.taskcla=taskcla
-        self.relu=torch.nn.ReLU()
+        # self.relu=torch.nn.ReLU()
         # self.drop=torch.nn.Dropout(0.5)
         self.fc1=torch.nn.Linear(ncha*size*size,400)
-        self.fc2=torch.nn.Linear(400,400)
+        # self.fc2=torch.nn.Linear(400,400)
 #         self.fc3=torch.nn.Linear(800,800)
 #         self.last=torch.nn.ModuleList()
 #         for t,n in self.taskcla:
@@ -23,8 +23,8 @@ class Net(torch.nn.Module):
 
     def forward(self,x):
         h=x.view(x.size(0),-1)
-        h=self.relu(self.fc1(h))
-        h=self.relu(self.fc2(h))
+        h=F.relu(self.fc1(h))
+        # h=self.relu(self.fc2(h))
 #         h=self.drop(self.relu(self.fc3(h)))
 #         y=[]
 #         for t,i in self.taskcla:
